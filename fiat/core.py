9155f03dfee0a7bc0966c85da8bda47f39b0d4b4
WithPrecedent <coreyrayburnyung@gmail.com>
2021-06-28 11:51:36 -0500

    added Director to core

--- fiat/core.py
+++ fiat/core.py
@@ -8,14 +8,14 @@ Contents:
 
 """
 from __future__ import annotations
-import abc
+import collections.abc
 import copy
 import dataclasses
 import itertools
-import multiprocessing
+from types import ModuleType
 from typing import (Any, Callable, ClassVar, Dict, Hashable, Iterable, List, 
-    Mapping, MutableMapping, MutableSequence, Optional, Sequence, Set, Tuple, 
-    Type, Union)
+                    Mapping, MutableMapping, MutableSequence, Optional, 
+                    Sequence, Set, Tuple, Type, Union)
 
 import more_itertools
 
